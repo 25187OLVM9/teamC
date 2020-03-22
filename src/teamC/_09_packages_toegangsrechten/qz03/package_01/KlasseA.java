78983d49b3589e736352e62523cbac4a5f3bb224
Jan-Willem Roode <JGPROODE@users.noreply.github.com>
2020-03-22 16:37:35 +0100

    antwoord verbeterd

--- src/teamC/_09_packages_toegangsrechten/qz03/package_01/KlasseA.java
+++ src/teamC/_09_packages_toegangsrechten/qz03/package_01/KlasseA.java
@@ -3,14 +3,19 @@ package teamC._09_packages_toegangsrechten.qz03.package_01;
 import teamC._09_packages_toegangsrechten.qz03.package_02.KlasseB;
 
 public class KlasseA
-{public int w = 1;
-protected int x = 2;
-int y = 3;
-private int z = 4;
-public static void main(String[] args)
 {
-	KlasseB kb = new KlasseB();
-	kb.mijnMethode();
-	
-}
+  public static void main(String[] args)
+  {
+    KlasseB kb = new KlasseB();
+	  
+	 
+    kb.mijnMethode();
+    System.out.println();
+	   //dit kan dus als je in KlasseB public gebruikt !
+	  
+    System.out.print(kb.w);
+    System.out.print(kb.x);
+    System.out.print(kb.y);
+    System.out.print(kb.z);
+  }
 }
