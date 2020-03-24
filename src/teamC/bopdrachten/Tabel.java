fd057731bb7e0d0795ec864218b10d5984bd2a92
rianneh <58731528+rianneh@users.noreply.github.com>
2020-03-24 12:42:50 +0100

    array tot 2000

--- src/teamC/bopdrachten/Tabel.java
+++ src/teamC/bopdrachten/Tabel.java
@@ -2,8 +2,24 @@ package teamC.bopdrachten;
 
 public class Tabel {
 
-	public void arraymethode() {
-
+	public void arraymethode() 
+	{
+		int e = 1;
+		int[] arrayInt = new int[2000];
+		
+		for(int i = 0; i < arrayInt.length; i ++)
+		{
+			arrayInt[i] = e;
+			System.out.print(arrayInt[i] + " | ");
+			 e ++;
+			 
+			 if( e == 1001)
+			 {
+				System.out.println("") ;
+			 }
+			 
+			 
+		}
 	}
 
 	public static void main(String[] args) {
