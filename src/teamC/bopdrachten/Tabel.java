afdbdf1e56ceb4121d5ac253afe648e5acfa16c8
rianneh <58731528+rianneh@users.noreply.github.com>
2020-03-24 11:42:23 +0100

    aanmaken tabel

---
+++ src/teamC/bopdrachten/Tabel.java
@@ -0,0 +1,29 @@
+package teamC.bopdrachten;
+
+public class Tabel {
+
+	public void arraymethode() {
+
+	}
+
+	public static void main(String[] args) {
+		Tabel ar = new Tabel();
+
+		ar.arraymethode();
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
