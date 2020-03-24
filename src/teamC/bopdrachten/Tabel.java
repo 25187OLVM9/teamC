4ca8374ab1eb25c8096f426a56c14d4e3545e10c
rianneh <58731528+rianneh@users.noreply.github.com>
2020-03-24 13:26:25 +0100

    tot 10.000

--- src/teamC/bopdrachten/Tabel.java
+++ src/teamC/bopdrachten/Tabel.java
@@ -5,7 +5,7 @@ public class Tabel {
 	public void arraymethode() 
 	{
 		int e = 1;
-		int[] arrayInt = new int[2000];
+		int[] arrayInt = new int[10000];
 		
 		for(int i = 0; i < arrayInt.length; i ++)
 		{
@@ -13,7 +13,7 @@ public class Tabel {
 			System.out.print(arrayInt[i] + " | ");
 			 e ++;
 			 
-			 if( e == 1001)
+			 if( e == 1001 || e == 2001 || e == 3001 || e ==4001 || e ==5001 || e == 6001 || e == 7001 || e == 8001 || e == 9001)
 			 {
 				System.out.println("") ;
 			 }
