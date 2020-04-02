16e8e501e5fe81a0987445c5e4060818e83fac0c
Stan <Stan@169.254.157.184>
2020-04-02 15:48:12 +0200

    Quiz 1 klaar

--- src/teamC/_11_statische_leden/qz01/Werknemer.java
+++ src/teamC/_11_statische_leden/qz01/Werknemer.java
@@ -14,6 +14,8 @@ public class Werknemer
     Werknemer wn1 = new Werknemer();
     Werknemer wn2 = new Werknemer();
     Werknemer wn3 = new Werknemer();
+    Werknemer wn4 = new Werknemer();
+    Werknemer wn5 = new Werknemer();
     System.out.print(Werknemer.stNr + ", ");
     System.out.print(wn1.nr + ", ");
     System.out.print(wn2.nr + ", ");
