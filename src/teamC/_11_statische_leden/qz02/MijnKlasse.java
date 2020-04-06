b95949c6e296fd22863f087886ac33afe53aca82
rianneh <58731528+rianneh@users.noreply.github.com>
2020-04-06 10:55:50 +0200

    gemaakt

--- src/teamC/_11_statische_leden/qz02/MijnKlasse.java
+++ src/teamC/_11_statische_leden/qz02/MijnKlasse.java
@@ -6,19 +6,21 @@ public class MijnKlasse
   public MijnKlasse()
   {
     x++;
+    x = 2; //toegevoegd
   }
   public static int mijnMethode(int i, int i2)
   {
     x += (i - i2);
     return x;
   }
+  
   public static void main(String[] args)
   {
     MijnKlasse mk1 = new MijnKlasse();
     MijnKlasse mk2 = new MijnKlasse();
     System.out.print(MijnKlasse.x + ", ");
     MijnKlasse mk3 = new MijnKlasse();
-    MijnKlasse.mijnMethode(8, 3);
+    MijnKlasse.mijnMethode(100, 10);
     System.out.print(MijnKlasse.x);
   }
 }
