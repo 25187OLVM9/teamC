48406fa3918cf68edd7922bcc112c6023dea98ee
Stan <Stan@169.254.157.184>
2020-04-22 11:39:12 +0200

    quiz 2

--- src/teamC/_13_final_klassen_final_sleutelwoord/qz02/MijnKlasse.java
+++ src/teamC/_13_final_klassen_final_sleutelwoord/qz02/MijnKlasse.java
@@ -2,13 +2,15 @@ package teamC._13_final_klassen_final_sleutelwoord.qz02;
 
 public class MijnKlasse extends Superklasse
 {
-  int z = 5;
-  void mijnMethode(int x)
-  {
-    System.out.print("x");
-  }
-  public static void main(String[] args)
-  {
-    MijnKlasse mk = new MijnKlasse();
-  }
+	int z = 5;	
+	
+	void mijnMethode(int x)
+	{
+		System.out.print("x");
+	}
+	public static void main(String[] args)
+	{
+		MijnKlasse mk = new MijnKlasse();
+	}
 }
+//We kunnen geen nieuwe methode methodeB toevoegen, ivm de final in de superklasse
