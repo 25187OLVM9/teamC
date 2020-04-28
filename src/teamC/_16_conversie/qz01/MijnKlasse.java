2a3610cac25935235fd713c2067072ce9a65cf87
Stan <Stan@169.254.157.184>
2020-04-28 11:36:58 +0200

    q1 af

--- src/teamC/_16_conversie/qz01/MijnKlasse.java
+++ src/teamC/_16_conversie/qz01/MijnKlasse.java
@@ -6,9 +6,13 @@ public class MijnKlasse
   {
     int i1 = 127;
     int i2 = 134;
+    int mijnInt = i1 + i2;
+    short mijnShort = i1;
     byte b1 = (byte) i1;
     byte b2 = (byte) i2;
     System.out.print(b1 + ", ");
     System.out.print(b2);
+    System.out.print(mijnInt);
+    //5: foutcode komt eruit
   }
 }
